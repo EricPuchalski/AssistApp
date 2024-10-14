a2c29c5130c0122d86dbc84353111923ec9c109c
Eric Puchalski <ericpuchalski12@gmail.com>
2024-10-13 21:38:11 -0300

    Update paginable
    Correciones en paginacion

--- front/assist_app/src/App.tsx
+++ front/assist_app/src/App.tsx
@@ -1,4 +1,5 @@
 import CreateStudent from './admin/CreateStudent';
+import Login from './admin/LogIn';
 import Managment from './admin/Managment';
 import UpdateStudent from './admin/UpdateStudent';
 import './App.css'
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Managment/>} />
         <Route path='/students/new' element={<CreateStudent/>}></Route>
         <Route path='/students/edit' element={<UpdateStudent/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
       </Routes>
     </Router>
     </>
